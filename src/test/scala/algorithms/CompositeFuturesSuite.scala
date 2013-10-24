dd295eb9dd23a786e28048cb66f512615b4d809a
prnicolas <pnicolas57@yahoo.com>
2013-10-24 00:01:38 -0700

    Initial commit for CompositeFuturesSuite

---
+++ src/test/scala/algorithms/CompositeFuturesSuite.scala
@@ -0,0 +1,20 @@
+/**
+ * Scala test suite for composable futures
+ * This code is provided solely for the purpose of illustrate concepts introduced or described 
+ * in some of the  of the post in the blog
+ * 
+ * @author Patrick Nicolas
+ */
+package algorithms
+
+
+import org.scalatest.FunSuite
+
+class CompositeFuturesSuite extends FunSuite {
+   test("Primary test case") {
+      val result =  new CompositeFutures(2, 2).compositeFuture
+      assert( result  == 57, "Futures composition result: " + result + " is incorrect")
+   }
+}
+
+// ---------------------------  EOF ---------------------------------------
