facbf68a53a46c5576e708b6dfd756f8013ed8eb
prnicolas <pnicolas57@yahoo.com>
2013-11-20 13:56:38 -0800

    Added continuation and currying examples for Slide presentation on Advanced functional programming in Scala

---
+++ src/main/scala/algorithms/Continuation.scala
@@ -0,0 +1,40 @@
+/**
+ * Classes that illustrates the use of delimited continuation in Scala
+ * This code is provided solely for the purpose of illustrate concepts introduced or described 
+ * in some of the  of the post in the blog
+ * 
+ * Note: The compiler require the CPS plug-in, using the continuations=enable in the command line
+ * 
+ * @author Patrick Nicolas
+ * @date October 7, 2013
+ * @see http://patricknicolas.blogspot.com
+ */
+package algorithms
+
+final class Computation( val f: Double=>Double) {
+  import scala.util.continuations._
+	
+  require(f != null, "cannot extend computation of undefined function")
+  
+  val identity = (h: Double => Double) => h
+
+  def continue(cont: (Double=>Double) => (Double=>Double) = identity): Double => Double = cont(f)
+   
+  def single(x: Double) = {
+	  reset {
+		  shift {h: (Double => Double) => h(x)*f(x) 
+	       } + x
+	   }
+   }
+   
+      
+   def nested(x: Double) = {
+	   reset {
+		   shift {h: (Double => Double) => { reset { shift { hh: (Double => Double) => h(hh(x*x))}} }
+	       } + f(x)
+	   }
+   }
+}
+
+
+// ------------------------  EOF -----------------------------------------
---
+++ src/main/scala/algorithms/Currying.scala
@@ -0,0 +1,40 @@
+/**
+ * Class to illustrate implementation of method currying using foldLeft high order functions
+ * This code is provided solely for the purpose of illustrate concepts introduced or described 
+ * in some of the  of the post in the blog
+ * 
+ * @author Patrick Nicolas
+ * @date October 2, 2013
+ * @see http://patricknicolas.blogspot.com
+ */
+package algorithms
+
+
+class CurriedCollection[T](val values: Array[T]) {
+   def foldLeft2[U](u: U, op: (U,T) => U): U = values.foldLeft[U](u)((u, t) => op(u,t))
+   def foldLeft[U](u: U)(op: (U,T) => U): U = this.foldLeft2(u, op)    
+}
+
+	/**
+	 * Singleton that illustrates the recursive implementation of
+	 * variable number of argument  f(x + (f(y) + f(z) + ...   ))))
+	 */
+object RecursiveCurrying {
+	def f(x: Double): Double = x*x
+	def f(x: Double, y: Double): Double = f(x + f(y))
+	def f(xs: Array[Double]) : Double = 
+	   if(xs.size > 1) f(xs.head + f(xs.takeRight(xs.size -1))) else xs(xs.size-1)
+}
+
+object Currying extends App {
+	val myCollection = new CurriedCollection[Int](Array[Int](3, 5, 8))
+	def sum(a: Int)(b: Int): Int = a+b
+	def sums(a: Int): Int => Int = b => sum(a)(b)
+	assert(sum(3)(5) == sums(3)(5), "Expected " + sums(3)(5) + " found " + sums(3)(5))
+
+	
+	println(RecursiveCurrying.f(2.0, 3.0))
+	println(RecursiveCurrying.f(Array[Double](2.0, 3.0, 5.0)))
+}
+
+// ----------------  EOF ---------------------------------------------------------
