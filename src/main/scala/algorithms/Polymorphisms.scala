1467222d62e5e0ff1c2f29bec62fd7766320c805
Patrick Nicolas <pnicolas57@yahoo.com>
2016-02-21 19:08:05 -0800

    This is a committed merge

--- src/main/scala/algorithms/Polymorphisms.scala
+++ src/main/scala/algorithms/Polymorphisms.scala
@@ -31,7 +31,7 @@ trait Concatenation[M[_]] {
 import scala.collection.GenSeq
 import scala.collection.mutable.{ArraySeq, ArrayBuffer}
 
-	// This is a F-bounded polymorphism  in PN2 branch
+	// This is a F-bounded polymorphism - merged
 trait Bucket[A <: Bucket[A]] extends Seq[A]
 
 
