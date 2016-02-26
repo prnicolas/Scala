f1844fb4de54dc20bb33d5646d28519366932af1
Patrick Nicolas <pnicolas57@yahoo.com>
2016-02-26 00:00:39 -0800

    Commit No 6

--- src/main/scala/algorithms/OptionPerformance.scala
+++ src/main/scala/algorithms/OptionPerformance.scala
@@ -11,7 +11,7 @@
 package algorithms
 
     /**
-    * This a master change No 5
+    * This a master change No 6
     */
 object OptionPerformance extends App {
 	def divOption(x : Double, y : Double) : Option[Double] = {
