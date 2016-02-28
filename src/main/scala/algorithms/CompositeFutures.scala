321974802ea5479a844714a1773c88cdb305add9
patrick.nicolas <patrick.nicolas@chartboost.com>
2016-02-27 16:10:39 -0800

    Changes made on EE

--- src/main/scala/algorithms/CompositeFutures.scala
+++ src/main/scala/algorithms/CompositeFutures.scala
@@ -3,7 +3,7 @@
  * Thread.sleep is used to emulate a lengthy computation
  * This code is provided solely for the purpose of illustrate concepts introduced or described 
  * in some of the  of the post in the blog
- * 
+ * Changes made on EE
  * @author Patrick Nicolas
  * @date July 23, 2013
  * @see http://patricknicolas.blogspot.com
